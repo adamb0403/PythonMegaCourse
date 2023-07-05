dcf5092b64148251c7a2323261b56cf46e2b21ca
Adam Butt <abutt@qti.qualcomm.com>
2023-07-05 17:14:43 +0100

    dicts&tuples

---
+++ create_dictionary.py
@@ -0,0 +1,7 @@
+day_temperatures = {"morning": 12.2, "noon": 12.5, "evening": 23.5}
+
+print(day_temperatures.values())
+
+day_temperatures_complex = {"morning": (1.7,2.8,3.9), "noon": (4.6,5.5,6.8), "evening": (7.8,8.8,9.8)} # Dict with tuples as values
+
+print(day_temperatures_complex.values())
---
+++ create_tuple.py
@@ -0,0 +1,3 @@
+color_codes = ((1,2,3),(4,5,6),(7,8,9)) # immutable list - cant append/remove
+
+print(color_codes)
